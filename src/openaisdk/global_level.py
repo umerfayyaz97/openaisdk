fb1be2341d2eea75946ba36f0ed17479b6b214d5
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-04-29 18:03:39 +0500

    global level

--- src/openaisdk/global_level.py
+++ src/openaisdk/global_level.py
@@ -3,32 +3,31 @@ from agents.run import RunConfig
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from agents import set_default_openai_client
 
+from dotenv import load_dotenv
+load_dotenv()
 
-
+#setting model and api key
+MODEL = "gemini/gemini-2.0-flash"
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
+# Set the default OpenAI client with the API key and base URL
 
 external_client = AsyncOpenAI(
     api_key=GEMINI_API_KEY,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
+set_default_openai_client(external_client)
+
 model = OpenAIChatCompletionsModel(
     model="gemini-2.0-flash",
     openai_client=external_client,
 )
 
-config = RunConfig(
-    model = model,
-    model_provider=external_client,
-    tracing_disabled = True,
-)
-
 agent = Agent(name="Assistant", instructions="Your name is Assistant. You are a helpful assistant.",)
 
-result = Runner.run_sync(agent, "What is the capital of France?", run_config=config)
+result = Runner.run_sync(agent, "What is the capital of France?")
 
 print(result.final_output)
-
-
-
