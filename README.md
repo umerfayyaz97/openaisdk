cd8b5e1d7aca544472c1f4bcc97793dc69dc30c1
umerfayyaz97 <115028799+umerfayyaz97@users.noreply.github.com>
2025-05-01 23:09:40 +0500

    level 2

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Learn To Add Different LLMs in OPENAISDK for Agent Development
+
+-Add .env file
+-Add GEMINI_API_KEY in env file (or use any other model and change)
--- src/openaisdk/global_level2.py
+++ src/openaisdk/global_level2.py
@@ -1,18 +1,19 @@
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled
-from agents.run import RunConfig
+from agents import Agent, Runner, AsyncOpenAI, set_default_openai_client, set_tracing_disabled, set_default_openai_api
 import os
 from dotenv import load_dotenv
 load_dotenv()
-from agents import set_default_openai_client
+
 
 from dotenv import load_dotenv
 load_dotenv()
 
+
 set_tracing_disabled(True)
 #setting model and api key
-MODEL = "gemini/gemini-2.0-flash"
+MODEL = "gemini-2.0-flash"
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
+set_default_openai_api("chat_completions")
 # Set the default OpenAI client with the API key and base URL
 
 external_client = AsyncOpenAI(
@@ -22,12 +23,9 @@ external_client = AsyncOpenAI(
 
 set_default_openai_client(external_client)
 
-model = OpenAIChatCompletionsModel(
-    model="gemini-2.0-flash",
-    openai_client=external_client,
-)
 
-agent = Agent(name="Assistant", instructions="Your name is Assistant. You are a helpful assistant.",)
+
+agent = Agent(name="Assistant", instructions="Your name is Assistant. You are a helpful assistant.", model=MODEL)
 
 result = Runner.run_sync(agent, "What is the capital of France?")
 
